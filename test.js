8572a7a4734b64d4990b6f3132b795173a7fef6d
August (Chris) <augu@voided.pw>
2020-01-10 12:54:36 -0700

    Added 2 new methods, add 2 more tests, update typings with JSDoc - Added 'hex' and 'bgHex' - Added test for the methods - Updated the typings with JSDoc - Updated 'rgbBg' and 'rgb' with array values for the first parameter

--- test.js
+++ test.js
@@ -15,3 +15,6 @@ console.log(leeks.eightBitBg('32', 'hello'));
 
 console.log(leeks.rgb('114', '137', '218', 'hello'));
 console.log(leeks.rgbBg('114', '137', '218', 'hello'));
+
+console.log(leeks.hex('#7289da', 'hello'));
+console.log(leeks.hexBg('#7289da', 'hello'));
