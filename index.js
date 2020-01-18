9a30bd3d2d049d61870451184e4781dfa6946097
David Ralph <ohlookitsderpy@protonmail.com>
2020-01-18 13:29:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ for (let c in d.colors) colors[c] = (t) => `\x1b[${d.colors[c]}m${t}\x1b[0m`;
 let styles = [];
 for (let s in d.styles) styles[s] = (t) => `\x1b[${d.styles[s]}m${t}\x1b[0m`;
 
-/** Check if colours are supported. (From https://github.com/jorgebucaran/colorette)*/
+/** 
+  * Check if colours are supported.
+  * @credit [Colorette](https://github.com/jorgebucaran/colorette)
+  */
 let supports = !('NO_COLOR' in process.env) && process.env.FORCE_COLOR !== '0' && (process.env.FORCE_COLOR || process.platform === 'win32' || (process.stdout.isTTY && process.env.TERM && process.env.TERM !== 'dumb'));
 
 /**
@@ -25,7 +28,7 @@ module.exports.eightBit = (i, t) => '\033' + `[38;5;${i}m${t}\x1b[0m`;
 
 /**
  * Change the background colour of the given text using 8-bit colours.
- * @param {string} i The 8-bit color to use
+ * @param {string} i The 8-bit colour to use
  * @param {string} t The text to show with the 8-bit colour
  */
 module.exports.eightBitBg = (i, t) => '\033' + `[48;5;${i}m${t}\x1b[0m`;
@@ -54,7 +57,7 @@ module.exports.rgbBg = (rgb, t) => {
  * Change the colour of the given text using hexadecimals.
  * @param {string} hex The hex to use
  * @param {string} t The text to show with the hexadecimal colour
- * @credit [Stackoverflow](https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb)
+ * @credit [Stack Overflow](https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb)
  */
 module.exports.hex = (hex, t) => {
 	const bigint = parseInt(hex.replace('#', ''), 16);
@@ -65,7 +68,7 @@ module.exports.hex = (hex, t) => {
  * Change the background colour of the given text using hexadecimals.
  * @param {string} hex The hex to use
  * @param {string} t The text to show with the hexadecimal colour
- * @credit [Stackoverflow](https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb)
+ * @credit [Stack Overflow](https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb)
  */
 module.exports.hexBg = (hex, t) => {
 	const bigint = parseInt(hex.replace('#', ''), 16);
